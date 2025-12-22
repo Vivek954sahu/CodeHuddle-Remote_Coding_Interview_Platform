75a0f07e71a8875eebdf59628689df674c83f573
vivek954sahu <vivek954sahu@gmail.com>
2025-12-22 11:50:20 +0530

    Add env variables loader

---
+++ backend/src/config/env.js
@@ -0,0 +1,38 @@
+import dotenv from "dotenv";
+
+export const loadEnv = () => {
+    dotenv.config();
+
+    const requiredEnv = [
+        "NODE_ENV",
+        "PORT",
+        "MONGODB_URI",
+        "JWT_SECRET",
+        "CLIENT_URL",
+
+        // GOOGLE OAUTH LOGIN
+        "GOOGLE_CLIENT_ID",
+        "GOOGLE_CLIENT_SECRET",
+
+        // STREAM VIDEO/AUDIO CALL
+        "STREAM_API_KEY",
+        "STREAM_API_SECRET",
+
+        // JUDGE0 CODE EXECUTER
+        "JUDGE0_API_URL",
+        "JUDGE0_API_KEY",
+
+        // INNGEST 
+        "INNGEST_EVENT_KEY",
+    ];
+
+    const missingEnv = requiredEnv.filter(
+        (key) => !process.env[key]
+    );
+
+    if(missingEnv.length > 0){
+        console.error("Missing required env variables:", missingEnv.join(", "));
+
+        process.exit(1);
+    }
+}
