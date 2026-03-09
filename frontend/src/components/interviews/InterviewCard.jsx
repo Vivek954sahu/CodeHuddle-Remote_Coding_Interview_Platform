81b254c07f888c9ebdc75f63ef9f75601ee584c4
vivek954sahu <vivek954sahu@gmail.com>
2026-03-09 12:40:30 +0530

    Vite config err fixing

---
+++ frontend/src/components/interviews/InterviewCard.jsx
@@ -0,0 +1,11 @@
+
+
+const InterviewCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InterviewCard
---
+++ frontend/src/pages/interview/InterviewRoom.jsx
@@ -0,0 +1,11 @@
+
+
+const InterviewRoom = () => {
+  return (
+    <div>
+      Interview Room
+    </div>
+  )
+}
+
+export default InterviewRoom
