28130830a96e34ae118b6d6f88d3f803eb5371c6
vivek954sahu <vivek954sahu@gmail.com>
2026-03-26 17:44:03 +0530

    Api call from frontend setup

--- frontend/src/api/interviewsApi.js
+++ frontend/src/api/interviewsApi.js
@@ -1,3 +1,13 @@
 import Axios from "./axios";
 
 export const getUpcomingInterviews = (params) => Axios.get("/v1/interviews/upcoming-interviews", {params});
+
+export const scheduleInterview = (data) => Axios.post("/v1/interviews", data);
+
+export const interviewById = (interviewId) => Axios.get(`/v1/interviews/${interviewId}/details`);
+
+export const joinInterview = (interviewId) => Axios.patch(`/v1/interviews/${interviewId}/join`);
+
+export const endInterview = (interviewId) => Axios.patch(`/v1/interviews/${interviewId}/end`);
+
+export const getStreamToken = () => Axios.get("/v1/interviews/stream-token");
