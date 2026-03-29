9d33e9ecedca497a5c2583a661c3b7791130c445
vivek954sahu <vivek954sahu@gmail.com>
2026-03-29 16:15:41 +0530

    Language Icon Error Fixed

--- frontend/src/components/interviewRoom/CodeEditor.jsx
+++ frontend/src/components/interviewRoom/CodeEditor.jsx
@@ -1,13 +1,17 @@
 import Editor from "@monaco-editor/react";
 import { LANGUAGE_CONFIG } from "../../data/language";
 import { LuLoaderCircle, LuPlay } from "react-icons/lu";
+import { IconBase } from "react-icons/lib";
 
 const CodeEditor = ({selectedLanguage, code, isRunning, onLanguageChange, onCodeChange, onRunCode }) => {
+
+  const icon = LANGUAGE_CONFIG[selectedLanguage]?.icon || null;
+
   return (
     <div className="h-full bg-base-300 flex flex-col">
       <div className="flex items-center justify-between px-4 py-3 bg-base-100 boirder-t border-base-300">
         <div className="flex items-center gap-3">
-            <div>{LANGUAGE_CONFIG[selectedLanguage].icon}</div>
+            <div>{icon && <IconBase/>}</div>
              <select value={selectedLanguage} onChange={onLanguageChange}>
                 {Object.entries(LANGUAGE_CONFIG).map(([key, lang]) => {
                     <option key={key} value={key}>
